afef8caf34f1fb2122503c8e4e046e676b21daf1
Vladimir Filipovic <vladofilipovic@hotmail.com>
2023-11-07 17:22:52 +0100

    dodao

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import rs.math.oop.g08.v01.kutije.Kutija;
+import rs.math.oop.g08.v01.kutije.ObojenaKutija;
+
+public class Main {
+    public static void main(String[] args) {
+        Kutija kut = new Kutija();
+        System.out.println(kut);
+        ObojenaKutija okut = new ObojenaKutija();
+        System.out.println(okut);
+    }
+}
