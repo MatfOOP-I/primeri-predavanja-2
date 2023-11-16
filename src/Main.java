4cce5f74d45580ad3b7cdb37c33aacf5f6bee971
Vladimir Filipovic <vladofilipovic@hotmail.com>
2023-11-16 09:23:15 +0100

    popravio

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import rs.math.oop.g08.v01.kutije.Kutija;
-import rs.math.oop.g08.v01.kutije.ObojenaKutija;
+
 
 public class Main {
     public static void main(String[] args) {
-        Kutija kut = new Kutija();
-        System.out.println(kut);
-        ObojenaKutija okut = new ObojenaKutija();
-        System.out.println(okut);
+        rs.math.oop.g02.p01.klaseObjekti.Student s = new rs.math.oop.g02.p01.klaseObjekti.Student();
+        s.ime = "Milos";
+        s.naBudzetu = false;
+        s.stampajPodatke();
     }
 }
